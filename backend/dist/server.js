1ce6d7a9ffffa26765c176c864312d9f8753f0b9
Piyush Bhatt <2023kuec2027@iiitkota.ac.in>
2026-05-19 00:44:48 +0530

    reset

--- backend/dist/server.js
+++
@@ -1,7 +0,0 @@
-import { createApp } from "./app.js";
-import { env } from "./config/env.js";
-const app = createApp();
-// Starts the HTTP server on the configured port.
-app.listen(env.port, () => {
-    console.log(`Backend server running on http://localhost:${env.port}`);
-});
--- frontend/src/main.tsx
+++ frontend/src/main.tsx
@@ -1,7 +1,6 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
-import { App } from "./App";
-import "./styles.css";
+import App from "./App";
 
 // Mounts the React application into the HTML root element.
 createRoot(document.getElementById("root")!).render(
